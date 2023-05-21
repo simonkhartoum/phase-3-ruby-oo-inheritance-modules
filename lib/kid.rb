e0df7539ce2e920f3e2a7ce3dd6cb635c8689f73
simon <simonkhartoum@gmail.com>
2023-05-21 23:00:37 +0300

    completed

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative '../lib/fancy_dance.rb'
+class Kid
+    extend MetaDancing
+    include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
